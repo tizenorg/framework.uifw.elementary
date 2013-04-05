db88a5164ca34235bc7fe3a36700446033721b6c
Thiep Ha <thiep.ha@samsung.com>
2013-04-05 13:47:46 +0900

    [Copy&Paste] Add notify when click on Copy/Cut item
    Change-Id: Iaae2abaa7ea5628b571efd5137bc48a4947a9ec7

--- src/lib/elm_module_priv.h
+++ src/lib/elm_module_priv.h
@@ -9,6 +9,7 @@ struct _Elm_Entry_Extension_data
 	Evas_Object *popup;
 	Evas_Object *ent;
 	Evas_Object *caller;
+	Evas_Object *notify; // TEMP: will be done by clipboard
 	Evas_Coord_Rectangle viewport_rect;
 	Eina_List *items;
 	cpfunc select;
