6851ab57d3fcb841eb21add94339b7255b4dde7d
Youngbok Shin <youngb.shin@samsung.com>
2013-05-28 20:21:34 +0900

    Entry: _elm_entry_smart_signali calls edje_object_message_signal_process for processing the pending messages. Because Edje can’t changes size or position of parts when it receive a signal without this.

--- src/lib/elm_entry.c
+++ src/lib/elm_entry.c
@@ -3233,9 +3233,13 @@ _elm_entry_smart_signal(Evas_Object *obj,
 
    /* always pass to both edje objs */
    edje_object_signal_emit(sd->entry_edje, emission, source);
+   edje_object_message_signal_process(sd->entry_edje);
 
    if (sd->scr_edje)
-     edje_object_signal_emit(sd->scr_edje, emission, source);
+     {
+        edje_object_signal_emit(sd->scr_edje, emission, source);
+        edje_object_message_signal_process(sd->scr_edje);
+     }
 }
 
 static void
